5d32315f847f023c3b386bf5e3100eb69866d8fa
hechaohe <550121308@qq.com>
2017-02-17 09:55:57 +0800

    Initial Commit

---
+++ test_HeightAndWidth/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test_HeightAndWidth
+//
+//  Created by hc on 2017/2/17.
+//  Copyright © 2017年 hc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ test_HeightAndWidth/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  test_HeightAndWidth
+//
+//  Created by hc on 2017/2/17.
+//  Copyright © 2017年 hc. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
